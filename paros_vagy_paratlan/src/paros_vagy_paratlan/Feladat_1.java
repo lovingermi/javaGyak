41c46cc144a1afb88d27fe6cb8eed4786282b83a
Miklós Lővinger <lovingermi@gmail.com>
2019-09-21 20:57:09 +0200

    Feladat_1 kész

--- paros_vagy_paratlan/src/paros_vagy_paratlan/Feladat_1.java
+++ paros_vagy_paratlan/src/paros_vagy_paratlan/Feladat_1.java
@@ -4,7 +4,23 @@ public class Feladat_1 {
 
 	public static void main(String[] args) 
 	{
-		//aaa
+		if (args.length==1)
+		{
+			int szam=Integer.parseInt(args[0]);
+			if (szam%2==0)
+			{
+				System.out.println(szam+" páros");
+			}
+			else
+			{
+				System.out.println(szam+" páratlan");
+			}
+		}
+		else
+		{
+			System.out.println("Egy paramétert kell megadni.");
+		}
+		
 
 	}
 
